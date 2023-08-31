b129b31fadf49381277c4d1ceb38da409a1a7d81
giorgio <giorgiobiologist@gmail.com>
2023-08-31 12:44:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ setup(
     author="Alberto Emiliani Giorgio Tassinari",   
     author_email="giorgio.programmer@gmail.com", 
     description="A simplyfy search on python",
-    long_description="qtag-SearchPyLinux is a search tool designed specifically for humans. It allows for searching files and directories based on tags - words or word beginnings - in any order.
-For example, searching with the tag -ari- would match -Aristotle-, my -Arist-, and even -MyFriendArist-, but not -parish-.",
+    long_description="qtag-SearchPyLinux is a search tool designed specifically for humans.",
     url="https://github.com/giorgioGTelian/SearchPyLinux",    
     packages=find_packages(),   
     install_requires=[
