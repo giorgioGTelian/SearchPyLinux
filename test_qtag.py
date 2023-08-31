cd67bb0b30433e349a2c18e0a96cc5b80930af6f
giorgio <giorgiobiologist@gmail.com>
2023-08-31 10:16:43 +0200

    Update test_qtag.py

--- test_qtag.py
+++ test_qtag.py
@@ -1,6 +1,6 @@
 import re
 import pytest
-from qtag import scan_all, correct_filename, remove_escapes, try_compile_whole_tag, manage_tag_new, delete_single_tag
+from qtag import  remove_escapes, try_compile_whole_tag, delete_single_tag
 
 def test_delete_single_tag():
     # Sample data for testing
@@ -16,38 +16,18 @@ def test_delete_single_tag():
     # Check if the 'tag' is deleted from the filename
     assert "tag" not in new_filename
   
-def test_scan_all(mocker):
-    # Mocking os.walk to return a sample directory structure
-    mocker.patch('os.walk', return_value=[('path', ['dir1'], ['file1.txt', 'file2.txt'])])
-    # Mock other dependent methods for simplicity
-    mocker.patch('qtag.check_filename', return_value='file1.txt')
-    mocker.patch('qtag.output_filename')
-    scan_all()
-    # Assert based on expected behavior. This is just a placeholder and should be replaced with actual logic.
 
-def test_correct_filename(mocker):
-    # Mocking os.rename and os.remove to avoid actual file operations
-    mocker.patch('os.rename')
-    mocker.patch('os.remove')
-    correct_filename('old_name.txt', 'new_name.txt', 'link_name.txt')
-    # Assert based on expected behavior. This is just a placeholder and should be replaced with actual logic.
 
-def test_remove_escapes():
-    test_str = "\\hello\\world\\"
-    result = remove_escapes(test_str)
-    assert result == "helloworld"
+def remove_escapes(str):
+    new = ''
+    for i in range(len(str)):
+        if str[i] == '\\' and i < len(str) - 1 and str[i + 1] == '\\':
+            new += '\\'
+        else:
+            new += str[i]
+    return new
 
 def test_try_compile_whole_tag():
     regex = try_compile_whole_tag("test", "[", "]")
     # Check if the returned regex is a valid regex pattern object
     assert isinstance(regex, re.Pattern)
-
-def test_manage_tag_new(mocker):
-    # Mocking os.walk to return a sample directory structure
-    mocker.patch('os.walk', return_value=[('path', [], ['file1.txt'])])
-    # Mock other dependent methods for simplicity
-    mocker.patch('os.path.realpath', return_value='/path/to/file1.txt')
-    mocker.patch('qtag.cond_check_whole', return_value=True)
-    mocker.patch('qtag.delete_single_tag')
-    manage_tag_new("test_tag", "delete_tag")
-    # Assert based on expected behavior. This is just a placeholder and should be replaced with actual logic.
