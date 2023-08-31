1f66efc12e6e904568d5938b249974e36fe79116
giorgio <giorgiobiologist@gmail.com>
2023-08-31 12:47:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SearchPyLinux",  
-    version="0.1.0",   
+    version="0.1.1",   
     author="Alberto Emiliani Giorgio Tassinari",   
     author_email="giorgio.programmer@gmail.com", 
     description="A simplyfy search on python",
